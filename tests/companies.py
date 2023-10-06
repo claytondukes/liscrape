09797c3c82819356ab87cd5658234a2480735121
Clayton Dukes <cdukes@logzilla.net>
2023-10-06 17:23:22 -0400

    fixed tests

--- tests/companies.py
+++ tests/companies.py
@@ -1,23 +1,37 @@
 import pytest
 
 from linkedin.spiders.companies import extracts_see_all_url
-from tests.selenium import SeleniumTest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
+# Your LinkedIn URLs
 GOOGLE = "https://www.linkedin.com/company/google"
 GOOGLE_USERS_LIST = "https://www.linkedin.com/search/results/people/?facetCurrentCompany=[%221441%22%2C%22621453%22%2C%22791962%22%2C%222374003%22%2C%2216140%22%2C%2210440912%22]"
 
+@pytest.fixture(scope="module")
+def driver():
+    # Initialize WebDriver
+    options = webdriver.ChromeOptions()
+    # If you need to add options, you can do so here
+    # For example: options.add_argument('--headless')
+    
+    driver = webdriver.Remote(
+        command_executor='http://selenium:4444',
+        options=options
+    )
+
+    # Yield the driver to the test function
+    yield driver
+
+    # Close the browser window
+    driver.quit()
 
 @pytest.mark.skip
-class CompaniesTest(SeleniumTest):
-    def setUp(self):
-        super().setUp()
-        # login(self.driver)
+def test_extracts_see_all_url(driver):
+    driver.get(GOOGLE)
+    url = extracts_see_all_url(driver)
+    print(url)
+    assert url.startswith(
+        "https://www.linkedin.com/search/results/people/?facetCurrentCompany="
+    )
 
-    @pytest.mark.skip
-    def test_extracts_see_all_url(self):
-        self.driver.get(GOOGLE)
-        url = extracts_see_all_url(self.driver)
-        print(url)
-        assert url.startswith(
-            "https://www.linkedin.com/search/results/people/?facetCurrentCompany="
-        )
--- tests/selenium.py
+++ tests/selenium.py
@@ -1,31 +1,47 @@
-import unittest
-
 import pytest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
-from linkedin.integrations.selenium import selenium_login
+# Update the URL and expected title
+TEST_URL = "http://www.google.com"
+EXPECTED_TITLE = "Google"
 
+@pytest.fixture(scope="module")
+def driver():
+    # Initialize WebDriver
+    options = webdriver.ChromeOptions()
+    # If you need to add options, you can do so here
+    
+    driver = webdriver.Remote(
+        command_executor='http://selenium:4444',
+        options=options
+    )
 
-@pytest.mark.skip
-class SeleniumTest(unittest.TestCase):
-    def setUp(self):
-        pass
-        # self.driver = init_chromium('localhost')
+    # Yield the driver to the test function
+    yield driver
 
-    def tearDown(self):
-        # pass
-        self.driver.close()
+    # Close the browser window
+    driver.quit()
 
+def test_google_search(driver):
+    """
+    Basic test to navigate to the Google page, perform a search, and check the title.
+    """
+    # Navigate to the test URL
+    driver.get(TEST_URL)
 
-class ChromiumTest(SeleniumTest):
-    @pytest.mark.skip
-    def test_init(self):
-        self.assertIsNotNone(self.driver)
-        print("type: %s" % type(self.driver))
+    # Check that the title of the page is as expected
+    assert EXPECTED_TITLE in driver.title
 
-    @pytest.mark.skip
-    def test_selenium_login(self):
-        selenium_login(self.driver)
+    # Find the search box using its name attribute value
+    search_box = driver.find_element("name", "q")
 
+    # Type 'Python' in the search box
+    search_box.send_keys("Python")
 
-if __name__ == "__main__":
-    unittest.main()
+    # Press the Enter key
+    search_box.send_keys(Keys.RETURN)
+
+    # Check that some results are displayed
+    assert "No results found." not in driver.page_source
+
